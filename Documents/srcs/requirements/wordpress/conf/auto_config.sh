979fbadc0b331d9f7bbcbb8ede3c83384a449a7a
Nathan FASCIA <nfascia@e3r9p6.clusters.42paris.fr>
2023-01-30 18:19:27 +0100

    coucou

--- Documents/srcs/requirements/wordpress/conf/auto_config.sh
+++ Documents/srcs/requirements/wordpress/conf/auto_config.sh
@@ -5,8 +5,9 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
 	--dbuser=$SQL_USER \
 	--dbpass=$SQL_PASSWORD \
 	--dbhost=mariadb:3306 --path='/var/www/wordpress'
-
+fi
 if [ ! -d /run/php ]; then
     mkdir ./run/php
-fi
+
 /usr/sbin/php-fpm7.3 -F
+fi

4172b5921875aa09ddc934fee6aa702f84c7f5e0
Nathan FASCIA <nfascia@e3r9p6.clusters.42paris.fr>
2023-01-30 17:20:25 +0100

    coucou

--- Documents/srcs/requirements/wordpress/conf/auto_config.sh
+++ Documents/srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,6 +1,12 @@
 sleep 10;
-wp config create --allow-root \
+if [ ! -e /var/www/wordpress/wp-config.php ]; then
+	wp config create --allow-root \
 	--dbname=$SQL_DATABASE \
 	--dbuser=$SQL_USER \
 	--dbpass=$SQL_PASSWORD \
 	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+if [ ! -d /run/php ]; then
+    mkdir ./run/php
+fi
+/usr/sbin/php-fpm7.3 -F

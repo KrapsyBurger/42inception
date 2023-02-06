4ed32bd70bf88a0b97105c626ac50ce74d777fd4
Nathan FASCIA <nfascia@e2r8p17.clusters.42paris.fr>
2023-02-06 17:35:15 +0100

    coucou

--- Documents/srcs/requirements/wordpress/tools/auto_config.sh
+++ Documents/srcs/requirements/wordpress/tools/auto_config.sh
@@ -10,22 +10,7 @@ wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_p
 fi
 
 
-# if /run/php folder does not exist, create it
 if [ ! -d /run/php ]; then
     mkdir ./run/php
 fi
 /usr/sbin/php-fpm7.3 -F
-
-#sleep 10;
-#if [ ! -e /var/www/wordpress/wp-config.php ]; then
-#	wp config create --allow-root \
-#	--dbname=$SQL_DATABASE \
-#	--dbuser=$SQL_USER \
-#	--dbpass=$SQL_PASSWORD \
-#	--dbhost=mariadb:3306 --path='/var/www/wordpress'
-#fi
-#if [ ! -d /run/php ]; then
-#    mkdir ./run/php
-    
-#/usr/sbin/php-fpm7.3 -F
-#fi
